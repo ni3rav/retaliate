0c90bbedf436903cbb1471f4f973c5b22115f717
ni3rav <mahetan35@gmail.com>
2025-05-08 13:14:55 +0530

    properly arranged props

--- retaliate.js
+++ retaliate.js
@@ -1,15 +1,17 @@
-//! things  to do 
+//! things  to do
 // * 1. implement createElement function that transpiles JSX to plain JS object
 //* 2. implement render function that takes the transpiled objects and creates a DOM node out of it
 
 const root = document.getElementById("root");
 
 //* this function will take type, props, children as arguments and return them in an object form which can be later used to create an element
-function createElement(type, prop, children) {
+function createElement(type, props, children) {
   return {
     type,
-    prop, //! it is and object
-    children, //! this can be an array with multiple entries
+    props: {
+      ...props,
+      children, //! this can be an array with multiple entries
+    }, //! it is and object
   };
 }
 
@@ -17,17 +19,17 @@ function createElement(type, prop, children) {
 
 function render(element) {
   const node = document.createElement(element.type);
-  node["class"] = element.prop.class;
+  node["className"] = element.props["className"];
 
   //* rendering the child, here it is only a string so we are creating a textNode
   const text = document.createTextNode("");
-  text["nodeValue"] = element.children;
+  text["nodeValue"] = element.props["children"];
 
   //* now appending the child to parent and parent to the document
   node.appendChild(text);
   root.appendChild(node);
 }
 
-const element = createElement("h1", { class: "skibidi" }, "hello");
+const element = createElement("h1", { className: "skibidi" }, "hello");
 console.log(element);
 render(element);
