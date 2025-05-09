cc849c9b598a200f90a9c2506a6609598c1c621b
ni3rav <mahetan35@gmail.com>
2025-05-09 10:12:36 +0530

    clean the retaliate script

--- retaliate.js
+++ retaliate.js
@@ -1,10 +1,3 @@
-//! things  to do
-// * 1. implement createElement function that transpiles JSX to plain JS object
-//* 2. implement render function that takes the transpiled objects and creates a DOM node out of it
-
-const root = document.getElementById("root");
-
-//* this function will take type, props, children as arguments and return them in an object form which can be later used to create an element
 function createElement(type, props, children) {
   return {
     type,
@@ -12,12 +5,11 @@ function createElement(type, props, children) {
       ...props,
       children: children.map((child) =>
         typeof child === "object" ? child : createTextElement(child)
-      ), //! this can be an array with multiple entries
-    }, //! it is and object
+      ),
+    },
   };
 }
 
-//* this function handles the creation of primitive node which in our case is a string
 function createTextElement(text) {
   return {
     type: "TEXT_ELEMENT",
@@ -27,8 +19,6 @@ function createTextElement(text) {
     },
   };
 }
-
-//* this function utilises the elemnt returned by the createElement function to render an actual node in the DOM
 function render(element, dom) {
   if (element.type === "TEXT_ELEMENT") {
     const node = document.createTextNode("");
@@ -38,27 +28,18 @@ function render(element, dom) {
   }
   const node = document.createElement(element.type);
 
-  //* filtering out all none children props and then applying them one by one with forEach loop
   Object.keys(element.props)
     .filter((key) => key !== "children")
     .forEach((name) => {
       node[name] = element.props[name];
     });
 
-  //* recursively calling the render function for each chld and since the child is inside of the element so we are changing the dom argument and passing our parent node here
   element.props.children.forEach((child) => render(child, node));
 
-  //* now appending the child to parent and parent to the document
   dom.appendChild(node);
 }
 
-const Retaliate = {
+export const Retaliate = {
   createElement,
   render,
 };
-
-const element = Retaliate.createElement("h1", { className: "skibidi" }, [
-  "hello",
-]);
-console.log(element);
-Retaliate.render(element, root);
