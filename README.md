85eb9256d4b91441aad7a2f49615657d1bcc2dc0
ni3rav <mahetan35@gmail.com>
2025-05-12 14:22:07 +0530

    add: implemented incremental asynchronous rendering than the synchronous recursive

--- README.md
+++ README.md
@@ -4,12 +4,28 @@
 - Implementation is done from this [awesome blog](https://pomb.us/build-your-own-react/) from [Rodrigo Pombo](https://twitter.com/pomber)
 
 ## Progress
+
 - [x] Implemented createElement function
 - [x] Implemented render function
 - [x] Implement JSX transpilation using esbuild to make it really React like
 - [x] Developement server with edit support along with build and dev command
+- [x] Incremental asynchronous rendering using `requestIdleCallback`,
+
+- The flow of the rendering can be visualised as follows
+
+![rendering flow](./rendering.png)
+
+- Here each node can have a parent, children, siblings which looks like this
+
+```
+        parent
+        |
+sibling-node-sibling
+        |
+        children
+```
 
 ## Problems with current implementation
-- Currently the entire rendering is done by synchronous recursive calls to render function which might be an issue for a bigger UI tree so I have to figure out a way to smartly delegate these calls such that UI can be rendered as quick as possible
-- Current state of this repo is no different than raw DOM manipulation, need to figure out some way to update the DOM smartly i.e. reconcilation (blog is getting a little bit tough too grasp from the fiber section onwards and I don't want to do just `copy-pasta`)
-- Implementing hook like behaviour for Components (`useState` as of now)
+
+- Lacks efficient diffing i.e. re-rendering whole dom in case of changes which is not ideal, it should update only the updated part
+- Lack of state (will start with implementing `useState` and then maybe `useEffect`)
--- retaliate.js
+++ retaliate.js
@@ -1,15 +1,19 @@
+//* creates a virtual dom element
 function createElement(type, props = {}, ...children) {
   return {
     type,
     props: {
       ...props,
-      children: children.flat().map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
+      children: children
+        .flat()
+        .map((child) =>
+          typeof child === "object" ? child : createTextElement(child)
+        ),
     },
   };
 }
 
+//* handles text nodes separately
 function createTextElement(text) {
   return {
     type: "TEXT_ELEMENT",
@@ -19,31 +23,141 @@ function createTextElement(text) {
     },
   };
 }
-function render(element, dom) {
-  
-  if (typeof element.type === "function") {
-    const componentElement = element.type(element.props);
-    render(componentElement, dom);
-    return;
-  }
 
-  if (element.type === "TEXT_ELEMENT") {
-    const node = document.createTextNode("");
-    node.nodeValue = element.props.nodeValue;
-    dom.appendChild(node);
-    return;
-  }
-  const node = document.createElement(element.type);
+//* holds the next unit of work for the fiber tree
+let nextUnitOfWork = null;
 
-  Object.keys(element.props)
+//* represents the root fiber we’re currently working on
+let wipRoot = null;
+
+//* creates fiber children from given virtual dom elements
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    //* new fiber for each child element
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: wipFiber,
+      dom: null,
+    };
+
+    //* if it's the first child, attach to fiber.child
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      //* else attach to previous sibling
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
+//* creates the real dom node from a fiber
+function createDom(fiber) {
+  const dom =
+    fiber.type === "TEXT_ELEMENT"
+      ? document.createTextNode("") //* text node
+      : document.createElement(fiber.type); //* regular element
+
+  //* set properties except children
+  Object.keys(fiber.props)
     .filter((key) => key !== "children")
     .forEach((name) => {
-      node[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => render(child, node));
+  return dom;
+}
 
-  dom.appendChild(node);
+//* commits the whole fiber tree to the real dom
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
+
+//* recursively appends fiber dom nodes to parent
+function commitWork(fiber) {
+  if (!fiber) return;
+
+  const parentDom = getParentDom(fiber);
+  if (fiber.dom) {
+    parentDom.appendChild(fiber.dom);
+  }
+
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+//* finds the nearest ancestor fiber that has a dom node
+function getParentDom(fiber) {
+  let parentFiber = fiber.parent;
+  while (!parentFiber.dom) {
+    parentFiber = parentFiber.parent;
+  }
+  return parentFiber.dom;
+}
+
+//* processes one fiber (a unit of work)
+function perfromUnitOfWork(fiber) {
+  const isFunctionComponent = typeof fiber.type === "function";
+
+  if (isFunctionComponent) {
+    //* call function component and reconcile returned elements
+    const children = [fiber.type(fiber.props)];
+    reconcileChildren(fiber, children);
+  } else {
+    //* create dom if not already created
+    if (!fiber.dom) {
+      fiber.dom = createDom(fiber);
+    }
+    //* reconcile its children
+    reconcileChildren(fiber, fiber.props.children);
+  }
+
+  //* return next unit of work in depth-first order
+  if (fiber.child) return fiber.child;
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+    nextFiber = nextFiber.parent;
+  }
+
+  return null;
+}
+
+//* main loop to do rendering work when the browser is idle
+function workLoop(deadline) {
+  //* keep working if we have work and there's time left
+  while (nextUnitOfWork && deadline.timeRemaining() > 1) {
+    nextUnitOfWork = perfromUnitOfWork(nextUnitOfWork);
+  }
+
+  //* if done building, commit the whole fiber tree
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  } else {
+    //* else keep requesting idle time to continue
+    requestIdleCallback(workLoop);
+  }
+}
+
+//* entry point to start rendering and sets up the root fiber
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: { children: [element] },
+    alternate: null,
+  };
+  nextUnitOfWork = wipRoot;
+  requestIdleCallback(workLoop);
 }
 
 export const Retaliate = {
